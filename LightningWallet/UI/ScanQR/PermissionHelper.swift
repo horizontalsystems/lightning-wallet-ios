287e2d869c6192e408ea90d1e0dfdc2dacd74527
anton <ant013@mail.ru>
2020-02-14 09:17:42 +0600

    Implement scan view with blur layer and handling camera permissions

---
+++ LightningWallet/UI/ScanQR/PermissionHelper.swift
@@ -0,0 +1,22 @@
+import UIKit
+import AVFoundation
+
+class PermissionsHelper {
+
+    static func performWithCameraPermission(onComplete: @escaping (Bool) -> ()) {
+        if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) == .authorized {
+            onComplete(true)
+        } else {
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { granted in
+                DispatchQueue.main.async {
+                    if granted {
+                        onComplete(true)
+                    } else {
+                        onComplete(false)
+                    }
+                }
+            })
+        }
+    }
+
+}
