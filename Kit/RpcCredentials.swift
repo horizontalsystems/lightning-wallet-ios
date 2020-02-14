4cf79c515cad8bdf85766e7feb02033562391514
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2020-02-14 16:38:14 +0600

    Ability to parse LndConnect URL to RpcCredentials instance

--- Kit/RpcCredentials.swift
+++ Kit/RpcCredentials.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public struct RpcCredentials {
     public let host: String
     public let port: Int
@@ -11,6 +13,24 @@ public struct RpcCredentials {
         self.macaroon = macaroon
     }
 
+    public init?(lndConnectUrlString: String) {
+        let string = lndConnectUrlString.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard let url = URL(string: string) else { return nil }
+        self.init(lndConnectUrl: url)
+    }
+
+    public init?(lndConnectUrl: URL) {
+        guard
+            let queryParameters = lndConnectUrl.queryParameters,
+            let host = lndConnectUrl.host,
+            let port = lndConnectUrl.port,
+            let macaroonString = queryParameters["macaroon"]?.base64UrlToBase64(),
+            let certificateString = queryParameters["cert"]?.base64UrlToBase64()
+            else { return nil }
+
+        self.init(host: host, port: port, certificate: Pem(key: certificateString).string, macaroon: macaroonString)
+    }
+
 }
 
 extension RpcCredentials: Codable {
@@ -22,4 +42,54 @@ extension RpcCredentials: CustomStringConvertible {
         "[host: \(host); port: \(port); certificate: \(certificate.count) char(s); macaroon: \(macaroon.count) char(s)]"
     }
 
+}
+
+fileprivate extension String {
+    func separate(every: Int, with separator: String) -> String {
+        let result = stride(from: 0, to: count, by: every)
+            .map { Array(Array(self)[$0..<min($0 + every, count)]) }
+            .joined(separator: separator)
+        return String(result)
+    }
+
+    func base64UrlToBase64() -> String {
+        var base64 = self
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+
+        if base64.count % 4 != 0 {
+            base64.append(String(repeating: "=", count: 4 - base64.count % 4))
+        }
+
+        return base64
+    }
+}
+
+fileprivate extension URL {
+    var queryParameters: [String: String]? {
+        guard
+            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems
+            else { return nil }
+
+        var parameters = [String: String]()
+        for item in queryItems {
+            parameters[item.name] = item.value
+        }
+        return parameters
+    }
+}
+
+fileprivate class Pem {
+    private let prefix = "-----BEGIN CERTIFICATE-----"
+    private let suffix = "-----END CERTIFICATE-----"
+    let string: String
+
+    init(key: String) {
+        if key.hasPrefix(prefix) {
+            string = key
+        } else {
+            string = "\(prefix)\n\(key.separate(every: 64, with: "\n"))\n\(suffix)\n"
+        }
+    }
 }
