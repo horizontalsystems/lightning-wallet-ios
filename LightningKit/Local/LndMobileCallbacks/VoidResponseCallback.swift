75180564f45716c6047c87887865084deae0eb65
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2020-02-28 15:43:51 +0600

    Remove emitter closure from LndmobileCallback instances
    - Lndmobile keeps(caches or for some other purpose) all the objects passed to it as callbacks. In our callback implementations VoidResponseCallback and MessageResponseCallback we need to release emitter(which holds a context from where it was created) when Single is completed

--- LightningKit/Local/LndMobileCallbacks/VoidResponseCallback.swift
+++ LightningKit/Local/LndMobileCallbacks/VoidResponseCallback.swift
@@ -2,7 +2,7 @@ import RxSwift
 import Lndmobile
 
 class VoidResponseCallback: NSObject, LndmobileCallbackProtocol {
-    private let emitter: ((SingleEvent<Void>) -> Void)?
+    private var emitter: ((SingleEvent<Void>) -> Void)?
     
     init(emitter: ((SingleEvent<Void>) -> Void)?) {
         self.emitter = emitter
@@ -10,9 +10,11 @@ class VoidResponseCallback: NSObject, LndmobileCallbackProtocol {
     
     func onError(_ error: Error?) {
         emitter?(.error(error ?? LndMobileCallbackError.unknownError))
+        emitter = nil
     }
     
     func onResponse(_ response: Data?) {
         emitter?(.success(Void()))
+        emitter = nil
     }
 }
